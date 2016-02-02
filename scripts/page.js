099bdade415b6371b58ace9b1adcc44ded1ade28
mgrinko <glukym@gmail.com>
2016-02-02 20:02:17 +0200

    Ajax was added

--- scripts/page.js
+++ scripts/page.js
@@ -7,6 +7,30 @@ module.exports = class Page {
   constructor(options) {
     this._el = options.element;
 
+    this._el.classList.add('loading');
+
+    var xhr = new XMLHttpRequest();
+
+    xhr.open('GET', './data/phones.json', true);
+
+    xhr.onload = function() {
+      this._el.classList.remove('loading');
+
+      if (xhr.status !== 200) {
+        alert( xhr.status + ': ' + xhr.statusText ); // пример вывода: 404: Not Found
+      } else {
+        var phones = JSON.parse(xhr.responseText);
+
+        this._init({
+          phones: phones
+        });
+      }
+    }.bind(this);
+
+    xhr.send();
+  }
+
+  _init(options) {
     this._filter = new Filter({
       element: this._el.querySelector('[data-component=filter]')
     });
@@ -20,6 +44,6 @@ module.exports = class Page {
   }
 
   _onPhoneSelected(event) {
-    alert(event.detail.phoneId + 13)
+    console.log('animation enabled');
   }
 };
